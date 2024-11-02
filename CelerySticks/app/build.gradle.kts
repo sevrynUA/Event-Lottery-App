2006149042a757a3f82add983d320586e8ffff68
bgreen0202 <bgreen@ualberta.ca>
2024-11-02 13:56:16 -0600

    Added QR Code Scanner UI, will need functions to navigate to QR code details when generator and QRcode is complete

--- CelerySticks/app/build.gradle.kts
+++ CelerySticks/app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 
 dependencies {
 
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation ("com.google.zxing:core:3.5.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
