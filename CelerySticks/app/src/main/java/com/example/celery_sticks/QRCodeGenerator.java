5b5ecaaff99f6be6a1f35ad6655d2fbf7aebb04e
Jared Granoski <jgranosk@ualberta.ca>
2024-11-07 15:51:37 -0700

    Database QR Code integration

--- CelerySticks/app/src/main/java/com/example/celery_sticks/QRCodeGenerator.java
+++ CelerySticks/app/src/main/java/com/example/celery_sticks/QRCodeGenerator.java
@@ -1,23 +1,36 @@
 package com.example.celery_sticks;
 
 import android.graphics.Bitmap;
+import android.util.Base64;
 import android.util.Log;
 
 import com.google.zxing.BarcodeFormat;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.ByteArrayOutputStream;
+
 public class QRCodeGenerator {
-    Bitmap qrcode;
-    public QRCodeGenerator() {
+    Bitmap qrCode;
+    String eventID;
+    ByteArrayOutputStream qrBytes;
+    byte[] data;
+    String encodedQR;
+    public QRCodeGenerator(String id) {
+        eventID = id;
     }
-    public Bitmap generate(String id) {
+    public String generate() {
         try {
             BarcodeEncoder encoder = new BarcodeEncoder();
-            qrcode = encoder.encodeBitmap(id, BarcodeFormat.QR_CODE, 400, 400);
+            qrCode = encoder.encodeBitmap(eventID, BarcodeFormat.QR_CODE, 400, 400);
         } catch(Exception e) {
-            Log.e("Error", "Generation failed" );
+            Log.e("QRCodeGenerator", "QR Code generation failed", e);
         }
-        return qrcode;
+        // bitmap to bytes adapted from https://stackoverflow.com/questions/4989182/converting-java-bitmap-to-byte-array
+        qrBytes = new ByteArrayOutputStream();
+        qrCode.compress(Bitmap.CompressFormat.PNG, 100, qrBytes);
+        data = qrBytes.toByteArray();
+        encodedQR = Base64.encodeToString(data, Base64.DEFAULT);
+        return encodedQR;
     }
 
 }
